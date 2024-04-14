e4de2e3125fd98405129531978cbc7f21e082297
Jahzara Taylor <85776056+JNyashaT@users.noreply.github.com>
2024-04-14 17:20:11 -0400

    Add files via upload

---
+++ Locations/Interactive Predictor Map.R
@@ -0,0 +1,16 @@
+setwd("C:/Users/jahza/PycharmProjects/POGOH Bikes Project/Pittsburgh Locations")
+library(sf)
+library(dplyr)
+library(mapview)
+
+
+assets = read.csv("assets.csv")
+supermarkets = read.csv("supermarkets.csv")
+terminals = read.csv("Terminal_Locations.csv")
+
+Assets = st_as_sf(assets, coords = c("longitude", "latitude"), crs = 4326)
+Supermarkets = st_as_sf(supermarkets, coords = c("longitude", "latitude"), crs = 4326)
+Terminals = st_as_sf(terminals, coords = c("Longitude", "Latitude"), crs = 4326)
+
+mapview(Assets, col.regions = "darkcyan") + mapview(Supermarkets, col.regions = "darkgoldenrod4") +
+  mapview(Terminals, col.regions = "darksalmon")
